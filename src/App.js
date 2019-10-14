971b27e6d464daa856da474148e282a9007d7fa5
dung1208 <52040970+dung1208@users.noreply.github.com>
2019-10-14 17:28:14 +0700

    1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from "./components/Header";
 import Screen1 from "./components/Screen1";
 import Screen2 from "./components/Screen2";
 import Screen3 from "./components/Screen3";
+import Screen4 from "./components/Screen4";
+
 class App extends Component {
   render () {
       return (
     <div className="App">
-        <Header />
+       <Header />
        <Screen1 />
        <Screen2 />
        <Screen3 />
+       <Screen4 />
     </div>
 
           );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import * as serviceWorker from './serviceWorker';
 
 
 
+window.onscroll = () => {
+    let header = document.getElementsByClassName('header');
+    let scrollTop = document.documentElement.scrollTop;
+        if(scrollTop ){
+            header[0].style.visibility = "visible";
+            }
+        else if (scrollTop === 0 ){
+            header[0].style.visibility = "hidden";
+            }
+}
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
